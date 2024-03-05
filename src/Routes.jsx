ebb8470362ef492a1e81a0ae6fe1d27d40dbb5e5
warrenshiv <warrenshiv@gmail.com>
2024-03-05 18:10:58 +0300

    Add Slider component and add the supplier pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,6 +6,10 @@ import ActivateDriverAccountVerifyBusiness from "./pages/Driver/ActivateDriverAc
 import DriverVehicleInformation from "./pages/Driver/DriverVehicleInformation";
 import DriverConnectWallet from "./pages/Driver/DriverConnectWallet";
 import DriverDashboard from "./pages/Driver/DriverDashboard";
+import ActivateSupplierAccountVerifyBusiness from "./pages/Supplier/ActivateSupplierAccountVerifyBusiness";
+import ActivateSupplierAccountConnectWallet from "./pages/Supplier/ActivateSupplierAccountConnectWallet"
+import CompanyOverview from "./pages/Supplier/CompanyOverview";
+import SupplierAccountSuccessfully from "./pages/Supplier/SupplierAccountSuccessfully";
 
 const ProjectRoutes = () => {
     let element = useRoutes([
@@ -32,7 +36,24 @@ const ProjectRoutes = () => {
         {
             path: "DriverDashboard",
             element: <DriverDashboard/>
+        },
+        {
+            path: "SupplierInformation",
+            element: <ActivateSupplierAccountVerifyBusiness />
+        },
+        {
+            path: "SupplierConnectWallet",
+            element: <ActivateSupplierAccountConnectWallet />
+        },
+        {
+            path: "CompanyOverview",
+            element: <CompanyOverview />
+        },
+        {
+            path: "SupplierAccountSuccessfully",
+            element: <SupplierAccountSuccessfully />
         }
+
     ]); 
     return element;
 }
--- src/pages/Driver/DriverDashboard/index.jsx
+++ src/pages/Driver/DriverDashboard/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Img } from "../../../components/Img";
 import { Text } from "../../../components/Text";
 import { Button } from "../../../components/Button";
